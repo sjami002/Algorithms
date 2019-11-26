8a61fed2e53a621b7bdfacd860bcde59c6833fc3
github username <sjami002@gmail.com>
2019-11-26 18:05:03 -0500

    stack

---
+++ Algos/stack.js
@@ -0,0 +1,24 @@
+// --- Examples
+//   const s = new Stack();
+//   s.push(1);
+//   s.push(2);
+//   s.pop(); // returns 2
+//   s.pop(); // returns 1
+
+class Stack {
+  constructor() {
+    this.data = [];
+  }
+
+  push(record) {
+    this.data.push(record);
+  }
+
+  pop() {
+    return this.data.pop();
+  }
+
+  peek() {
+    return this.data[this.data.length - 1];
+  }
+}
